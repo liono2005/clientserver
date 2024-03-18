2c07653a693c46e02935d9066803706dd8023ed4
lino2005 <rejmerleonid@gmail.com>
2024-03-18 14:52:28 +0300

    dfsgr

---
+++ src/main/java/com/example/client2/MainController.java
@@ -0,0 +1,88 @@
+package com.example.client2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class MainController {
+
+    @FXML
+    private TableView<?> bookTable;
+
+    @FXML
+    private TableColumn<?, ?> columnAuthor;
+
+    @FXML
+    private TableColumn<?, ?> columnGenre;
+
+    @FXML
+    private TableColumn<?, ?> columnNumber;
+
+    @FXML
+    private TableColumn<?, ?> columnPublisher;
+
+    @FXML
+    private TableColumn<?, ?> columnTitle;
+
+    @FXML
+    void addBookAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addOrChangeAuthorAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addOrChangeCityAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addOrChangeGenreAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addOrChangePublisherAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeBookAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteAuthorAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteBookAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteCityAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteGenreAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletePublisherAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/client2/Start.java
@@ -0,0 +1,7 @@
+package com.example.client2;
+
+public class Start {
+    public static void main(String[] args) {
+        MainApplication.main(args);
+    }
+}
